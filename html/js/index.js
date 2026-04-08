44b6b962833cc1e426621c5c5aef00d592c40766
Nolan Stone <nolan@3xion.dev>
2026-04-08 10:20:19 -0400

    init 3

--- html/js/index.js
+++ html/js/index.js
@@ -1,6 +1,6 @@
 function toggleCite() {
     if (document.getElementById("works-cited-content").innerHTML == "Click to reveal references") {
-        document.getElementById("works-cited-content").innerHTML = "https://learning.mheducation.com/ <br>https://en.wikipedia.org/wiki/Presidency_of_Thomas_Jefferson <br>https://www.whitehousehistory.org/bios/thomas-jefferson <br>https://www.presidency.ucsb.edu/documents/thomas-jefferson-event-timeline"
+        document.getElementById("works-cited-content").innerHTML = "https://learning.mheducation.com/ <br>https://en.wikipedia.org/wiki/American_Civil_War <br>https://www.whitehousehistory.org/bios/abraham-lincoln <br>https://www.battlefields.org/learn/articles/trigger-events-civil-war <br>https://en.wikipedia.org/wiki/Origins_of_the_American_Civil_War"
     } else {
         document.getElementById("works-cited-content").innerHTML = "Click to reveal references"
     }
